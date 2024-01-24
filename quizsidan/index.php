fba3bda6a2f3e3f2fe041fe9e886424a3ce8e122
arvidwg <arvidwg@varmdogymnasium.se>
2024-01-24 17:02:09 +0100

    fixat så det är helt klart för responsiv

--- quizsidan/index.php
+++ quizsidan/index.php
@@ -10,7 +10,7 @@ include('check_login.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hemsida</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
@@ -27,13 +27,16 @@ include('check_login.php');
                 <p class="lead">Välkommen tillbaka <?php echo $_SESSION['name']; ?>!</p>
                 <div class="row">
 
-                    <div class="col-8">
+                    <div class="col">
                         <h1>Alla Quizer</h1>
                     </div>
                     <div class="col m-auto">
                         <a href="create_quiz.php">
-                            <button type="button" class="btn btn-primary m-0">
-                                Gör en quiz
+                            <button type="button" id="makeQuizBtn" class="btn float-end d-none d-md-block btn-lg btn-primary rounded-pill m-0">
+                                Skapa en quiz
+                            </button>
+                            <button type="button" id="makeQuizBtn" class="btn float-end d-sm-block d-xs-block d-md-none btn-primary rounded-pill m-0">
+                                Skapa en quiz
                             </button>
                         </a>
                     </div>
@@ -58,14 +61,14 @@ include('check_login.php');
 
                             for ($i = 0; $i < $rows_nr; $i++) {
                                 echo "<div class='col p-2'>";
-                                echo "<button class='card p-5 w-100 h-100 bg-body-tertiary' type='submit' name='quiz' value='" . $rows[$i]['id'] . "'>";                                // echo "<img src='#' class='card-img-top' alt='#'>";
+                                echo "<button class='card border-0 p-4 w-100 h-100 bg-body-tertiary' type='submit' name='quiz' value='" . $rows[$i]['id'] . "'>";                                // echo "<img src='#' class='card-img-top' alt='#'>";
                                 // echo "<div style='width: 100%; height: 100px; background-color: #000000;'></div>";
                                 // echo "<hr>";
                                 echo "<div class='m-auto'>";
-                                echo "<h4 class='card-title mb-0'>" . $rows[$i]['name'] . "</h4>";
+                                echo "<h4 class='card-title mx-2 mb-0'>" . $rows[$i]['name'] . "</h4>";
                                 echo "</div>";
                                 if ($websettings['showCreatorName'] == 1) {
-                                    echo "<div class='small text-body-secondary'>Skapad av: " . $rows[$i]['creator'] . "</div>";
+                                    echo "<div class='small m-auto text-body-secondary'>Skapad av: " . $rows[$i]['creator'] . "</div>";
                                 }
                                 echo "</button>";
                                 echo "</div>";
